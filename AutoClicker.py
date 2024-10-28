654127928a55af310f3bfe6369e955c90267ce5f
DavidNis <davnis37@gmail.com>
2024-10-28 08:27:05 +0200

    finished auto clicker

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,1 +1,34 @@
+import pyautogui
+import time
+import keyboard
 
+running = True
+paused = False
+
+def stop_prog():
+    global running
+    running = False
+    print("Exiting program.")
+
+def pause_prog():
+    global paused
+    paused = not paused
+    print("Paused" if paused else "Resumed")
+
+keyboard.add_hotkey('ctrl+q', stop_prog)
+keyboard.add_hotkey('ctrl+s', pause_prog)
+
+print("Press ctrl+q to stop the program")
+print("Press ctrl+s to pause the program")
+time.sleep(2)
+
+
+while running:
+    if not paused:
+        pyautogui.click()
+        print("Clicked!")
+        
+    for _ in range(300): 
+        if not running:
+            break
+        time.sleep(1)
